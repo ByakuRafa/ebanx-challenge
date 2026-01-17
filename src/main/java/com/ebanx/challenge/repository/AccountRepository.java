5d32a5b3b33e7f166053fdca69841c4c9d1f970e
Rafael Blanco <36845448+ByakuRafa@users.noreply.github.com>
2026-01-17 10:24:20 -0300

    Add AccountRepository

---
+++ src/main/java/com/ebanx/challenge/repository/AccountRepository.java
@@ -0,0 +1,30 @@
+package com.ebanx.challenge.repository;
+import com.ebanx.challenge.model.Account;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class AccountRepository {
+    private final Map<String, Account> database = new ConcurrentHashMap<>();
+
+    public Optional<Account> findById(String id){
+        Account account = database.get(id);
+        if (account == null){
+             return Optional.empty();
+            }
+        return Optional.of(null);
+    }
+
+    public Account save(Account acciut){
+        return acciut;
+    }
+
+    public void clear(){
+        database.clear();
+    }
+
+   
+}
