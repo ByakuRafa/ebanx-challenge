2cf4a62b652c45cac2fd515988fb8e544f693cf2
Rafael Blanco <36845448+ByakuRafa@users.noreply.github.com>
2026-01-17 10:24:38 -0300

    Update Account.java

--- src/main/java/com/ebanx/challenge/model/Account.java
+++ src/main/java/com/ebanx/challenge/model/Account.java
@@ -1,5 +1,10 @@
 package com.ebanx.challenge.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Account {
-
+    private String id;
+    private Double balance;
 }
