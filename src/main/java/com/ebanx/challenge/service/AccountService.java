1b84ac1cd9a30f75a8750e7d3390759b88c9dbfc
Rafael Blanco <36845448+ByakuRafa@users.noreply.github.com>
2026-01-18 20:19:07 -0300

    Update AccountService.java

--- src/main/java/com/ebanx/challenge/service/AccountService.java
+++ src/main/java/com/ebanx/challenge/service/AccountService.java
@@ -15,5 +15,11 @@ public class AccountService {
     public AccountService(AccountRepository repository){
         this.repository = repository;
     }
+
+    public Double getBalance(String accountId){
+        Optional<Account> account = repository.findById(accountId);
+        return account.map(Account::getBalance).orElse(0.0);
+    }
+
 }
 
