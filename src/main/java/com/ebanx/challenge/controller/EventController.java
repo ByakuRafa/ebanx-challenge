7692e4e1b766509bada3f9a71c9731a3597fbc9f
Rafael Blanco <36845448+ByakuRafa@users.noreply.github.com>
2026-01-18 20:19:17 -0300

    Create EventController.java

---
+++ src/main/java/com/ebanx/challenge/controller/EventController.java
@@ -0,0 +1,23 @@
+package com.ebanx.challenge.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.ebanx.challenge.service.AccountService;
+
+import org.springframework.http.HttpStatus;
+
+@RestController
+public class EventController {
+
+    private final AccountService service;
+
+    public EventController(AccountService service){
+        this.service = service;
+    }
+
+    public ResponseEntity<Doble> getBalance(@RequestParam("account_id") String accountID){
+        Double balance = service.getBalance(accountID);
+        return ResponseEntity.ok(balance);
+    }
+}
