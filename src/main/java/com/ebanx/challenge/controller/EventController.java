b93207afa2b405d2092ccdd338a98aa459dd899f
Rafael Blanco <36845448+ByakuRafa@users.noreply.github.com>
2026-01-18 21:32:29 -0300

    Update EventController.java

--- src/main/java/com/ebanx/challenge/controller/EventController.java
+++ src/main/java/com/ebanx/challenge/controller/EventController.java
@@ -3,9 +3,13 @@ package com.ebanx.challenge.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.ebanx.challenge.dto.EventRequest;
 import com.ebanx.challenge.service.AccountService;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import java.util.Map;
 
 @RestController
 public class EventController {
@@ -16,10 +20,24 @@ public class EventController {
         this.service = service;
     }
 
+    @GetMapping("/balance")
     public ResponseEntity<Double> getBalance(@RequestParam("account_id") String accountID){
         Double balance = service.getBalance(accountID);
         return ResponseEntity.ok(balance);
     }
 
+    public ResponseEntity<Object> handleEvent(@RequestBody EventRequest request){
+        
+        //deposit
+        if ("deposit".equals(request.getType())){
+            var account = service.deposit(request.getDestination(), request.getAmount());
+            return new ResponseEntity<>(Map.of("destination", account),HttpStatus.CREATED);
+            
+        };
+
+        //out of scope requests
+        return ResponseEntity.badRequest().build();
+        
+    }
  
 }
